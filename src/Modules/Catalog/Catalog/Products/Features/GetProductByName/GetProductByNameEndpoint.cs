97f619b347ecd7d773182e56fb32f583ebe63177
KietTuan <theonlytruth25012005@gmail.com>
2026-06-07 14:55:41 +0700

    Change docker config to runable, add Get product by name API
    - Not use volumes in postgres_eshopdb in override docker file
    - Add SEQ_FIRSTRUN_ADMINPASSWORD for "seq container"
    -  Add get products by name, create two file: GetProductByNameEndpoint and  GetProductByNameHandler

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductByName/GetProductByNameEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.Products.Features.GetProductByName;
+
+//public record GetProductByNameRequest(string Name);
+public record GetProductByNameResponse(IEnumerable<ProductDto> Products);
+
+public class GetProductByNameEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/name/{Name}", async (string Name, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByNameQuery(Name));
+
+            var response = result.Adapt<GetProductByNameResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductByName")
+        .Produces<GetProductByNameResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Name")
+        .WithDescription("Get Product By Name");
+    }
+}
---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductByName/GetProductByNameHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Catalog.Products.Features.GetProductByName;
+
+public record GetProductByNameQuery(string Name)
+    : IQuery<GetProductByNameResult>;
+public record GetProductByNameResult(IEnumerable<ProductDto> Products);
+
+internal class GetProductByNameHandler(CatalogDbContext dbContext)
+    : IQueryHandler<GetProductByNameQuery, GetProductByNameResult>
+{
+    public async Task<GetProductByNameResult> Handle(GetProductByNameQuery query, CancellationToken cancellationToken)
+    {
+        // get products by Name using dbContext
+        // return result
+
+        var products = await dbContext.Products
+                .AsNoTracking()
+                .Where(p => p.Name.Contains(query.Name))
+                .OrderBy(p => p.Name)
+                .ToListAsync(cancellationToken);
+
+        //mapping product entity to productdto
+        var productDtos = products.Adapt<List<ProductDto>>();
+
+        return new GetProductByNameResult(productDtos);
+    }
+}
